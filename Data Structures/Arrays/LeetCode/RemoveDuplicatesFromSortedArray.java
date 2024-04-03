9a3d7909d53d291ae745dcc964adc7c6aa09b9ec
Siddhant <siddhantkadam881@gmail.com>
2024-04-03 14:24:06 +0530

    start

---
+++ Data Structures/Arrays/LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count = 1;
+        int rem = 0;
+        for(int i =0;i<nums.length-1 ; i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {   
+                nums[count++] = nums[i+1];
+                
+            }
+            
+        }
+        return count;
+    }
+}
